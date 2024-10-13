06c3b0c7681f23f2b21d00dcac315598bd608a46
PrajwalPatil-01 <prajwalpatilxp@gmail.com>
2024-10-13 07:31:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/PrajwalPatil-01/XP Universe /XP Universe 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /XP Universe 
 fi
-cd /Advance-File-Share-Bot
+cd /XP Universe 
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
